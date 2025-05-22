f175976e00ee1e1bc17fb934d5d39464c7eaf41c
SaltyBid <47874855+jasonrod1@users.noreply.github.com>
2025-05-22 09:47:43 -0500

    Create consts.py

---
+++ custom_components/apex_monitor/consts.py
@@ -0,0 +1,8 @@
+SENSOR_MAP = {
+    "Tmp": ("Apex Temp", "°F"),
+    "pH": ("Apex pH", None),
+    "ORPx4": ("Apex ORP", "mV"),
+    "Alkx3": ("Apex Alk", "dKH"),
+    "Cax3": ("Apex Calcium", "ppm"),
+    "Mgx3": ("Apex Magnesium", "ppm")
+}
